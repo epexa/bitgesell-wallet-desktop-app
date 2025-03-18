aa9f55757b9fa798882accbf5cc2593b0fdf82d8
epexa@bk.ru <epexa@bk.ru>
2025-03-18 18:23:14 +0300

    update

--- src/main.js
+++ src/main.js
@@ -8,6 +8,17 @@ const { autoUpdater } = require('electron-updater');
 
 let mainWindow;
 
+/* const upsertKeyValue = (obj, keyToChange, value) => {
+	const keyToChangeLower = keyToChange.toLowerCase();
+	for (const key of Object.keys(obj)) {
+		if (key.toLowerCase() === keyToChangeLower) {
+			obj[key] = value;
+			return;
+		}
+	}
+	obj[keyToChange] = value;
+}; */
+
 const createWindow = () => {
 	mainWindow = new BrowserWindow({
 		width: 1280,
@@ -17,18 +28,60 @@ const createWindow = () => {
 		icon: `${__dirname}/icons/256.png`,
 		webPreferences: {
 			devTools: false,
-			webSecurity: false,
+			enableRemoteModule: false,
+			sandbox: true,
+			webSecurity: true,
 			webgl: false,
 			contextIsolation: true,
 			spellcheck: false,
 			enableWebSQL: false,
+			nodeIntegration: false,
+			plugins: false,
+			experimentalFeatures: false,
+			allowRunningInsecureContent: false,
 		},
 	});
 
 	mainWindow.loadFile('www/index.html');
 
+	/* START FIX CORS */
+	/* mainWindow.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
+		const { requestHeaders } = details;
+		upsertKeyValue(requestHeaders, 'Access-Control-Allow-Origin', [ '*' ]);
+		callback({ requestHeaders });
+	});
+	mainWindow.webContents.session.webRequest.onHeadersReceived((details, callback) => {
+		const { responseHeaders } = details;
+		upsertKeyValue(responseHeaders, 'Access-Control-Allow-Origin', [ '*' ]);
+		upsertKeyValue(responseHeaders, 'Access-Control-Allow-Headers', [ '*' ]);
+		callback({ responseHeaders });
+	}); */
+
+	// for electron >= 23
+	/* mainWindow.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
+		const { requestHeaders } = details;
+		upsertKeyValue(requestHeaders, 'Origin', '*');
+		upsertKeyValue(requestHeaders, 'Sec-Fetch-Site', 'none');
+		upsertKeyValue(requestHeaders, 'Sec-Fetch-Mode', 'no-cors');
+		upsertKeyValue(requestHeaders, 'Sec-Fetch-Dest', 'document');
+		callback({ requestHeaders });
+	}); */
+	/* END FIX CORS */
+
 	Menu.setApplicationMenu(null);
 };
+
+app.disableHardwareAcceleration();
+app.commandLine.appendSwitch('disable-audio');
+app.commandLine.appendSwitch('disable-wasm');
+
+app.on('certificate-error', (event, webContents, url, error, certificate, callback) => {
+	if (url.startsWith('https://api.bitaps.com')) {
+		event.preventDefault();
+		callback(true);
+	}
+	else callback(false);
+});
 
 app.whenReady().then(() => {
 	createWindow();
